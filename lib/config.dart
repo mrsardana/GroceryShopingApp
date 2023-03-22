c68ec7187fa97c73960683cbe2b1d62043f2321f
Deepak Sardana <deepaksardana03@gmail.com>
2023-03-21 20:29:03 -0400

    deployed to heroku

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,9 @@
 class Config {
   static const String appName = "Grocery App";
   // static const String apiURL = "10.0.0.154:4000";
-  static const String apiURL = "127.0.0.1:4000";
+  static const String apiURL = "grocery-shopping-app.herokuapp.com";
   // static const String imageURL = "http://10.0.0.154:4000";
-  static const String imageURL = "http://127.0.0.1:4000";
+  static const String imageURL = "";
   static const String categoryAPI = "api/category";
   static const String productAPI = "api/product";
   static const String registerAPI = "api/register";
