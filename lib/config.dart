09dea2ecd6d9eca2c713d5b8b0e465b5d861ab91
Deepak Sardana <deepaksardana03@gmail.com>
2023-04-03 19:41:09 -0400

    d

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,7 @@
 class Config {
   static const String appName = "Grocery App";
-  // static const String apiURL = "localhost:4000";
-  static const String apiURL = "grocery-shopping-app.herokuapp.com";
-  // static const String imageURL = "http://10.0.0.154:4000";
+  static const String apiURL = "localhost:4000";
+  // static const String apiURL = "grocery-shopping-app.herokuapp.com";
   static const String imageURL = "";
   static const String categoryAPI = "api/category";
   static const String productAPI = "api/product";
